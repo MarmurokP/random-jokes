324a32e9abbbba8457124887f67777ad00c6da11
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-01-23 00:34:08 +0300

    feat: add hero, skills, portfolio sections

--- portfolio/index.js
+++ portfolio/index.js
@@ -5,8 +5,8 @@ const menu = document.querySelector('.header-nav');
 const menuClasses = menu.classList;
 
 burgerItem.addEventListener('click', () => {
-    burgerClasses.toggle('burger-cross');
+    burgerClasses.toggle('burger-cross');   
     menuClasses.toggle('close');
-    menuClasses.toggle('open');
-})
+    menuClasses.toggle('open');        
+});
 
