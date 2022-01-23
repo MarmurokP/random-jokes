e1f674c053b35d9347295380accc385f4143a891
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-01-23 23:28:32 +0300

    fix: burger css and js

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,10 +3,19 @@ const burgerItem = document.querySelector('.burger');
 const burgerClasses = burgerItem.classList;
 const menu = document.querySelector('.header-nav');
 const menuClasses = menu.classList;
+const navLink = menu.querySelectorAll('.nav-link');
 
 burgerItem.addEventListener('click', () => {
     burgerClasses.toggle('burger-cross');   
     menuClasses.toggle('close');
-    menuClasses.toggle('open');        
+    menuClasses.toggle('open');            
 });
 
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        burgerClasses.remove('burger-cross');
+        menuClasses.remove('open');
+        burgerClasses.add('burger');
+        menuClasses.add('close');
+    });
+})
