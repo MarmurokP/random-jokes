9aa441ec6da557d464ed83009af540ed6e49939c
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-02-13 21:58:16 +0300

    feat: add english quotes function

---
+++ random-jokes/index.js
@@ -0,0 +1,28 @@
+const randomQuotUrl = 'https://type.fit/api/quotes';
+const randomText = document.querySelector('.text');
+const quotAutor = document.querySelector('.author');
+const quotButton = document.querySelector('.change-joke');
+
+function showData(data) {
+    let qoutNum = Math.floor(0 + Math.random() * (data.length + 1));
+  
+    randomText.textContent = `"${data[qoutNum].text}"`;
+    data[qoutNum].author === null ? quotAutor.textContent = `- Unknown` : quotAutor.textContent = `- ${data[qoutNum].author}`;     
+};
+
+async function getData() {
+    const res = await fetch(randomQuotUrl);
+    const data = await res.json();
+    
+    showData(data);
+    
+  };
+  getData();
+
+  quotButton.addEventListener('click', getData);
+
+  
+  
+ 
+
+ 
